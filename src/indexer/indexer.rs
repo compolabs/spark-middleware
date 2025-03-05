57ca129ada7b3094ad09e5063c3c1889b837bc62
nazgull08 <nazgull08@protonmail.com>
2025-03-05 12:59:51 +0300

    cleanup

--- src/indexer/indexer.rs
+++
@@ -1,8 +0,0 @@
-use crate::error::Error;
-use async_trait::async_trait;
-use tokio::task::JoinHandle;
-
-#[async_trait]
-pub trait Indexer: Send + Sync {
-    async fn initialize(&self, tasks: &mut Vec<JoinHandle<()>>) -> Result<(), Error>;
-}
